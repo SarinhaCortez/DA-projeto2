5d235ff2e0403c1cddf29199bb4618687fef1514
Marta Silva <up202208258@fe.up.pt>
2024-05-09 11:52:42 +0100

    nearest neighbour algorithm

--- Utils.h
+++ Utils.h
@@ -4,7 +4,31 @@
 #include "data_structures/MatrixGraph.h"
 #include <vector>
 #include <set>
+#include <iostream>
+using namespace std;
 
 bool BacktrakingTSP(MGraph &g, int &dist, int &minDist, std::vector<int> &tsp, std::vector<int> &final, int &numVertex, int vertex, bool&found, std::set<int> &visited);
+
+bool notPresent(int i, std::vector<int> &tsp);
+pair<int, int> minDist(std::vector<double> currentRow, std::vector<int> &tsp);
+bool nearestNeighbour(MGraph &g,  std::vector<int> &tsp, std::vector<std::vector<double>>* matrix);
+
+
+/*
+double distanceNodes(pair<int, std::pair<double, double>>& point1, pair<int, std::pair<double, double>>& point2);
+void assignClusters(std::vector<pair<int, std::pair<double, double>>>& data,  vector<pair<int, std::pair<double, double>>>& centroids, vector<pair<int, int>>& labels);
+void updateCentroids(vector<pair<int, std::pair<double, double>>>& data, vector<pair<int, std::pair<double, double>>>& centroids, vector<pair<int, int>>& labels);
+void kmeans(std::vector<pair<int, std::pair<double, double>>> &data, int k, int maxIterations);
+double distance(const pair<int, std::pair<double, double>>& p1, const pair<int, std::pair<double, double>>& p2);
+std::vector<int> solveTSP(const std::vector<pair<int, std::pair<double, double>>>& points);
+vector<vector<int>> solveTSPForEachCluster(std::vector<pair<int, std::pair<double, double>>>& data,
+                            std::vector<pair<int, std::pair<double, double>>>& centroids,
+                            std::vector<pair<int, int>>& labels);
+*/
+
+
+
+
+
 
 #endif //DA_PROJETO2_UTILS_H
